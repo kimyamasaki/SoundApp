9dae1286a1d3d2e17785faeb715b28065975df7b
Kimalye <kimalye@gmail.com>
2015-04-06 00:21:12 -0700

    Loading Screen Added!

---
+++ app/js/loading.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function(){
+  var loadScreen = document.getElementById("loadScreen");
+  setTimeout(function(){
+        document.body.removeChild(loadScreen);
+    }, 5000);
+  
+});
