b55b8ce5884c3a8e33976b7569226d466d977dc6
Kimalye <kimalye@gmail.com>
2015-04-05 19:01:20 -0700

    Testing Shake Function

--- app/js/app.js
+++ app/js/app.js
@@ -37,7 +37,30 @@ window.onload = function() {
         $('#gradient').fadeOut();
     });
 
+    var myShakeEvent = new Shake({
+        threshold: 15
+    });
+
+    // start listening to device motion
+    myShakeEvent.start();
+
+    // register a shake event
+    window.addEventListener('shake', shakeEventDidOccur, false);
+
+    //shake event callback
+    function shakeEventDidOccur () {
+
+        //put your own code here etc.
+        alert('Shake! Clearing? Hopefully ;a;');
+        angular.element(document.getElementById('shakeMe')).scope().clearAll();
+        angular.element(document.getElementById('shakeMe')).scope().newSoundParty();
+
+
+    }
+
 };
+
+
 
 
 
