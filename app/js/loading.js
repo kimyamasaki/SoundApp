575c54f97d0168ed1ad50757716c2972f4a7006a
Kimalye <kimalye@gmail.com>
2015-04-06 23:57:36 -0700

    final2

--- app/js/loading.js
+++ app/js/loading.js
@@ -2,6 +2,6 @@ window.addEventListener("load", function(){
   var loadScreen = document.getElementById("loadScreen");
   setTimeout(function(){
         document.body.removeChild(loadScreen);
-    }, 200);
+    }, 1000);
   
 });
